947c79fcea18d9e444dd28fd64e9378388e22c3d
STDano <sashadano2003@gmail.com>
2025-03-14 15:40:22 +0800

    weatherApp2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,30 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  location: any = [];
 
   constructor() {}
 
+  async getCurrentLocation() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      const { latitude, longitude } = coordinates.coords;
+
+      this.location.latitude = latitude;
+      this.location.longitude = longitude;
+    } catch(error) {
+      alert("Error getting location: "+error);
+    }
+  }
+
+  async watchLocation() {
+      const watchID = Geolocation.watchPosition({}, (position,err) =>  {
+        if(err) {
+          console.log("Error watching postion: ", err);
+          return
+        }
+        console.log("Watched Position: ", position);
+        this.location = position?.coords;
+      });
+    }
 }
