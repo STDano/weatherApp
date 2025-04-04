0ca75424b424f5e8e0ac2d2fdc208d3cbd2ae140
STDano <sashadano2003@gmail.com>
2025-04-04 16:43:35 +0800

    More functions

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,33 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-location: any;
+private locationSubject = new BehaviorSubject<{ lat: number, lon: number} | null>(null);
+location: Observable<{ lat: number, lon: number } | null> = this.locationSubject.asObservable();
 
   constructor() { }
 
   async getCurrentLocation() {
-    try {
-      const coordinates = await Geolocation.getCurrentPosition();
+      try {
+        const location = await Geolocation.getCurrentPosition();
 
-      const { latitude, longitude } = coordinates.coords;
-      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
-    } catch(error) {
-      alert("Error getting location: "+error);
-    }
+        const coords = {
+          lat: location.coords.latitude,
+          lon: location.coords.longitude
+        };
+
+        this.locationSubject.next(coords);
+        return coords;
+
+      } catch (error) {
+        console.error('Error getting location: ', error);
+        return;
+      }
   }
 
-  async watchLocation() {
-      const watchID = Geolocation.watchPosition({}, (position,err) =>  {
-        if(err) {
-          console.log("Error watching postion: ", err);
-          return
+  async trackCurrentLocation() {
+      Geolocation.watchPosition (
+        { enableHighAccuracy: true },
+        (location, error) => {
+          if(error) {
+            console.error('Error tracking current location: ')
+            return;
+          }
+          if(location) {
+            const coords = {
+              lat: location.coords.latitude,
+              lon: location.coords.longitude
+            };
+
+            this.locationSubject.next(coords);
+            console.log('Location updated: ', coords)
+          }
         }
-        console.log("Watched Position: ", position);
-        this.location = position?.coords;
-      });
+      )
     }
 }
--- src/app/services/preferences.service.ts
+++ src/app/services/preferences.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,31 @@ import { Injectable } from '@angular/core';
 export class PreferencesService {
 
   constructor() { }
+
+  async getPreference(key: string): Promise<any> {
+    const preference = (await Preferences.get({ key: key }))?.value;
+
+    if(preference) {
+      try {
+        return JSON.parse(preference);
+      } catch(error) {
+        return preference;
+      }
+    }
+
+    return null;
+  }
+
+  async createPreference(key: string, value: any) {
+    try {
+      await Preferences.set({ key: key, value: JSON.stringify(value) })
+    } catch {
+      await Preferences.set({ key: key, value: value });
+    }
+  }
+
+  async clearPreferences() {
+    await Preferences.clear();
+    alert("Preferences cleared");
+  }
 }
